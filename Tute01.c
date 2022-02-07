6e541bff7012d85f6c0dc96bc74435d993c51c1d
ishara tharindu <97008963+dissnayaka@users.noreply.github.com>
2022-02-07 22:14:14 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2;
+	float avg;
+	
+	printf("Enter tha mark 1 :- ");
+	scanf("%d",&mark1);
+	
+	printf("Enter tha mark 2 :- ");
+	scanf("%d",&mark2);
+	
+	avg= (mark1+mark2)/2.0;
+	
+	printf("Average mark is :- %.1f",avg);
   
   return 0;
 }
